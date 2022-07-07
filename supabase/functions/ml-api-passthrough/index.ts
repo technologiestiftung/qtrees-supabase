5d534bbb6a0b23d546ff4356a6a110cdeca6168d
ff6347 <hash@ff6347.email>
2022-07-07 13:16:53 +0200

    docs: Add some test requests

--- supabase/functions/ml-api-passthrough/index.ts
+++ supabase/functions/ml-api-passthrough/index.ts
@@ -2,19 +2,25 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-import { serve } from "https://deno.land/std@0.131.0/http/server.ts"
+import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
+// import { verify } from "https://deno.land/x/djwt@v2.7/mod.ts";
 const ml_pgrest_host = Deno.env.get("ML_PGREST_HOST");
 const ml_pgrest_user = Deno.env.get("ML_PGREST_USER");
 const ml_pgrest_password = Deno.env.get("ML_PGREST_PASSWORD");
 const ml_pgrest_port = Deno.env.get("ML_PGREST_PORT");
-const jwt_secret = Deno.env.get("JWT_SECRET");
+const jwt_secret = Deno.env.get("SB_JWT_SECRET");
 import { supabaseClient } from "../_shared/supabase-client.ts";
 
-if (ml_pgrest_host === undefined || ml_pgrest_user === undefined || ml_pgrest_password === undefined || ml_pgrest_port === undefined || jwt_secret === undefined) {
-  console.error("Missing environment variables: ML_PGREST_HOST, ML_PGREST_USER, ML_PGREST_PASSWORD, ML_PGREST_PORT, JWT_SECRET");
+if (
+  ml_pgrest_host === undefined || ml_pgrest_user === undefined ||
+  ml_pgrest_password === undefined || ml_pgrest_port === undefined ||
+  jwt_secret === undefined
+) {
+  console.error(
+    "Missing environment variables: ML_PGREST_HOST, ML_PGREST_USER, ML_PGREST_PASSWORD, ML_PGREST_PORT, JWT_SECRET",
+  );
   Deno.exit(1);
 }
-
 
 // find limit in url.search and replace with mx of 100000
 function set_limit(url: URL, amount: number): URL {
@@ -27,23 +33,64 @@ function set_limit(url: URL, amount: number): URL {
     url.searchParams.set("limit", `${amount}`);
   }
   return url;
+
 }
-const body_not_authorized = JSON.stringify({ message: "Not authorized" })
-const header_not_authorized = { headers: { 'Content-Type': 'application/json' }, status: 401 };
+const body_internal_server_error = JSON.stringify({ message: "internal server error" });
+const body_not_authorized = JSON.stringify({ message: "Not authorized" });
+const header_not_authorized = {
+  headers: { "Content-Type": "application/json" },
+  status: 401,
+};
+const header_internal_server_error = {
+  headers: { "Content-Type": "application/json" },
+  status: 500,
+};
 const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey',
-}
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey",
+};
 
 serve(async (req: Request) => {
   // This is needed if you're planning to invoke your function from a browser.
   if (req.method === "OPTIONS") {
-    return new Response('ok', { headers: corsHeaders })
+    return new Response("ok", { headers: corsHeaders });
   }
 
-  const session = supabaseClient.auth.setAuth(req.headers.get("Authorization")!.replace("Bearer ", ""))
-  const { data, error } = await supabaseClient.from('profiles').select('*')
-  console.log({ data, error });
+  const session = supabaseClient.auth.setAuth(
+    req.headers.get("Authorization")!.replace("Bearer ", ""),
+  );
+  // server side user verfification
+  // https://github.com/supabase/supabase/issues/147#issuecomment-949953227
+
+  const { data: user, error: user_error } = await supabaseClient.auth.api
+    .getUser(session.access_token);
+
+  if (user_error) {
+    return new Response(body_internal_server_error, header_internal_server_error);
+  }
+  if (!user) {
+    return new Response(body_not_authorized, header_not_authorized);
+  }
+
+  // console.log({ token: session.access_token, jwt_secret, user });
+  const { data: profile, error: error_profile } = await supabaseClient.from("profiles").select(
+    `
+      id,
+      user_roles (
+        role, user_id
+      )
+    `,
+  ).eq("id", user.id).single();
+
+  if (error_profile) {
+    return new Response(body_internal_server_error, header_internal_server_error);
+  }
+  if (!profile) {
+    return new Response(body_not_authorized, header_not_authorized);
+  }
+  if (!profile.user_roles.some((user_role: { role: "admin" | "viewer" | "editor" }) => user_role.role === "admin")) {
+    return new Response(body_not_authorized, header_not_authorized);
+  }
 
   // if (session. ) {
   //   return new Response(body_not_authorized, header_not_authorized)
@@ -55,13 +102,12 @@ serve(async (req: Request) => {
   //   console.log(user);
   // }
 
-
   let url = new URL(req.url);
   url = set_limit(url, 100_000);
 
-  const target_url = `${ml_pgrest_host}:${ml_pgrest_port}${url.pathname}${url.search}`;
+  const target_url =
+    `${ml_pgrest_host}:${ml_pgrest_port}${url.pathname}${url.search}`;
   const res = await fetch(target_url);
-
 
   if (!res.ok) {
     console.error(`Error fetching ${ml_pgrest_host}:${ml_pgrest_port}`);
@@ -70,21 +116,21 @@ serve(async (req: Request) => {
   const json = await res.json();
 
   if (url.pathname === "/") {
-
-    const paths = Object.keys(json.paths).filter(p => p !== "/" && p !== "/rpc/login");
+    const paths = Object.keys(json.paths).filter((p) =>
+      p !== "/" && p !== "/rpc/login"
+    );
     if (!paths.includes(url.pathname)) {
-      return new Response(JSON.stringify({ paths }), { headers: { 'Content-Type': 'application/json' } });
+      return new Response(JSON.stringify({ paths }), {
+        headers: { "Content-Type": "application/json" },
+      });
     }
   }
-
-
-
 
   return new Response(
     JSON.stringify(json),
     { headers: { "Content-Type": "application/json" } },
-  )
-})
+  );
+});
 
 // To invoke:
 // curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
