0903465cf1a50850c4b400ce1b6f8249c4ee0bf7
ff6347 <hash@ff6347.email>
2022-07-20 20:01:43 +0200

    feat: Add gml id restriction
    remove auth code
    
    
    # No more than 50 chars. #### 50 chars is here: #
    # <type>[optional scope]: <description>
    # feat(SCOPE):
    # fix(SCOPE):
    # chore(SCOPE):
    # docs(SCOPE):
    # style(SCOPE):
    # refactor(SCOPE):
    # perf(SCOPE):
    # test(SCOPE):
    # ci(SCOPE):
    
    # Remember blank line between title and body.
    # Body: Explain *what* and *why* (not *how*). Include task ID (Jira issue).
    # Wrap at 72 chars. ################################## which is here: #
    # BREAKING CHANGE: xyz
    #
    # At the end: Include Co-authored-by for all contributors.
    # Include at least one empty line before it. Format:
    # Co-authored-by: name <user@users.noreply.github.com>
    #
    # How to Write a Git Commit Message:
    # https://chris.beams.io/posts/git-commit/
    #
    # 1.Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how

--- supabase/functions/_shared/supabase-client.ts
+++ supabase/functions/_shared/supabase-client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@1.35.4";
 
 export const supabaseClient = createClient(
-  // Supabase API URL - env var exported by default when deployed.
-  Deno.env.get("SUPABASE_URL") ?? "",
-  // Supabase API ANON KEY - env var exported by default when deployed.
-  Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+	// Supabase API URL - env var exported by default when deployed.
+	Deno.env.get("SUPABASE_URL") ?? "",
+	// Supabase API ANON KEY - env var exported by default when deployed.
+	Deno.env.get("SUPABASE_ANON_KEY") ?? "",
 );
--- supabase/functions/ml-api-passthrough/index.ts
+++ supabase/functions/ml-api-passthrough/index.ts
@@ -2,134 +2,165 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
+import { serve } from "./deps.ts";
+import { set_limit } from "./utils.ts";
 // import { verify } from "https://deno.land/x/djwt@v2.7/mod.ts";
 const ml_pgrest_host = Deno.env.get("ML_PGREST_HOST");
 const ml_pgrest_user = Deno.env.get("ML_PGREST_USER");
 const ml_pgrest_password = Deno.env.get("ML_PGREST_PASSWORD");
 const ml_pgrest_port = Deno.env.get("ML_PGREST_PORT");
 const jwt_secret = Deno.env.get("SB_JWT_SECRET");
-import { supabaseClient } from "../_shared/supabase-client.ts";
+// import { supabaseClient } from "../_shared/supabase-client.ts";
 
 if (
-  ml_pgrest_host === undefined || ml_pgrest_user === undefined ||
-  ml_pgrest_password === undefined || ml_pgrest_port === undefined ||
-  jwt_secret === undefined
+	ml_pgrest_host === undefined ||
+	ml_pgrest_user === undefined ||
+	ml_pgrest_password === undefined ||
+	ml_pgrest_port === undefined ||
+	jwt_secret === undefined
 ) {
-  console.error(
-    "Missing environment variables: ML_PGREST_HOST, ML_PGREST_USER, ML_PGREST_PASSWORD, ML_PGREST_PORT, JWT_SECRET",
-  );
-  Deno.exit(1);
+	console.error(
+		"Missing environment variables: ML_PGREST_HOST, ML_PGREST_USER, ML_PGREST_PASSWORD, ML_PGREST_PORT, JWT_SECRET",
+	);
+	Deno.exit(1);
 }
 
-// find limit in url.search and replace with mx of 100000
-function set_limit(url: URL, amount: number): URL {
-  const limit = url.searchParams.get("limit");
-  if (limit) {
-    if (parseInt(limit, 10) > amount) {
-      url.searchParams.set("limit", `${amount}`);
-    }
-  } else {
-    url.searchParams.set("limit", `${amount}`);
-  }
-  return url;
+// const body_internal_server_error = JSON.stringify({
+// 	message: "internal server error",
+// });
+// const body_not_authorized = JSON.stringify({ message: "Not authorized" });
+// const header_not_authorized = {
+// 	headers: { "Content-Type": "application/json" },
+// 	status: 401,
+// };
 
-}
-const body_internal_server_error = JSON.stringify({ message: "internal server error" });
-const body_not_authorized = JSON.stringify({ message: "Not authorized" });
-const header_not_authorized = {
-  headers: { "Content-Type": "application/json" },
-  status: 401,
+const body_missing_gml_id_param = JSON.stringify({
+	message: "missing gml_id search param",
+});
+const header_wrong_request = {
+	headers: { "Content-Type": "application/json" },
+	status: 400,
 };
+
 const header_internal_server_error = {
-  headers: { "Content-Type": "application/json" },
-  status: 500,
+	headers: { "Content-Type": "application/json" },
+	status: 500,
 };
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey",
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Headers": "authorization, x-client-info, apikey",
 };
 
 serve(async (req: Request) => {
-  // This is needed if you're planning to invoke your function from a browser.
-  if (req.method === "OPTIONS") {
-    return new Response("ok", { headers: corsHeaders });
-  }
+	try {
+		// This is needed if you're planning to invoke your function from a browser.
+		if (req.method === "OPTIONS") {
+			return new Response("ok", { headers: corsHeaders });
+		}
 
-  const session = supabaseClient.auth.setAuth(
-    req.headers.get("Authorization")!.replace("Bearer ", ""),
-  );
-  // server side user verfification
-  // https://github.com/supabase/supabase/issues/147#issuecomment-949953227
+		/*
+  // This block of code adds auth using supabase js sdk
+  // not needed right now but good to keep around for the next iteration
 
-  const { data: user, error: user_error } = await supabaseClient.auth.api
-    .getUser(session.access_token);
 
-  if (user_error) {
-    return new Response(body_internal_server_error, header_internal_server_error);
-  }
-  if (!user) {
-    return new Response(body_not_authorized, header_not_authorized);
-  }
+	const session = supabaseClient.auth.setAuth(
+	  req.headers.get("Authorization")!.replace("Bearer ", ""),
+	);
+	server side user verfification
+	https://github.com/supabase/supabase/issues/147#issuecomment-949953227
 
-  // console.log({ token: session.access_token, jwt_secret, user });
-  const { data: profile, error: error_profile } = await supabaseClient.from("profiles").select(
-    `
-      id,
-      user_roles (
-        role, user_id
-      )
-    `,
-  ).eq("id", user.id).single();
+	const { data: user, error: user_error } = await supabaseClient.auth.api
+	  .getUser(session.access_token);
 
-  if (error_profile) {
-    return new Response(body_internal_server_error, header_internal_server_error);
-  }
-  if (!profile) {
-    return new Response(body_not_authorized, header_not_authorized);
-  }
-  if (!profile.user_roles.some((user_role: { role: "admin" | "viewer" | "editor" }) => user_role.role === "admin")) {
-    return new Response(body_not_authorized, header_not_authorized);
-  }
+	if (user_error) {
+	  return new Response(body_internal_server_error, header_internal_server_error);
+	}
+	if (!user) {
+	  return new Response(body_not_authorized, header_not_authorized);
+	}
 
-  // if (session. ) {
-  //   return new Response(body_not_authorized, header_not_authorized)
-  // }
-  // if (!user) {
-  //   console.error("User not found");
-  //   return new Response(body_not_authorized, header_not_authorized);
-  // } else {
-  //   console.log(user);
-  // }
+	console.log({ token: session.access_token, jwt_secret, user });
+	const { data: profile, error: error_profile } = await supabaseClient.from("profiles").select(
+	  `
+	    id,
+	    user_roles (
+	      role, user_id
+	    )
+	  `,
+	).eq("id", user.id).single();
 
-  let url = new URL(req.url);
-  url = set_limit(url, 100_000);
+	if (error_profile) {
+	  return new Response(body_internal_server_error, header_internal_server_error);
+	}
+	if (!profile) {
+	  return new Response(body_not_authorized, header_not_authorized);
+	}
+	if (!profile.user_roles.some((user_role: { role: "admin" | "viewer" | "editor" }) => user_role.role === "admin")) {
+	  return new Response(body_not_authorized, header_not_authorized);
+	}
 
-  const target_url =
-    `${ml_pgrest_host}:${ml_pgrest_port}${url.pathname}${url.search}`;
-  const res = await fetch(target_url);
+	if (session. ) {
+	  return new Response(body_not_authorized, header_not_authorized)
+	}
+	if (!user) {
+	  console.error("User not found");
+	  return new Response(body_not_authorized, header_not_authorized);
+	} else {
+	  console.log(user);
+	}
+  */
 
-  if (!res.ok) {
-    console.error(`Error fetching ${ml_pgrest_host}:${ml_pgrest_port}`);
-    Deno.exit(1);
-  }
-  const json = await res.json();
+		let url = new URL(req.url);
 
-  if (url.pathname === "/") {
-    const paths = Object.keys(json.paths).filter((p) =>
-      p !== "/" && p !== "/rpc/login"
-    );
-    if (!paths.includes(url.pathname)) {
-      return new Response(JSON.stringify({ paths }), {
-        headers: { "Content-Type": "application/json" },
-      });
-    }
-  }
+		const searchParams = url.searchParams;
+		if (
+			url.pathname.replace("/ml-api-passthrough", "") === "/trees" &&
+			!searchParams.has("gml_id")
+		) {
+			return new Response(body_missing_gml_id_param, header_wrong_request);
+		}
+		url = set_limit(url, 100_000);
 
-  return new Response(
-    JSON.stringify(json),
-    { headers: { "Content-Type": "application/json" } },
-  );
+		const target_url = `${ml_pgrest_host}:${ml_pgrest_port}${url.pathname.replace(
+			"/ml-api-passthrough",
+			"",
+		)}${url.search}`;
+
+		const res = await fetch(target_url);
+
+		if (!res.ok) {
+			const message = await res.text();
+			console.error(
+				`Error fetching ${ml_pgrest_host}:${ml_pgrest_port} status: ${res.status} message: ${message}`,
+			);
+
+			return new Response(
+				JSON.stringify({ message }),
+				header_internal_server_error,
+			);
+		}
+		const json = await res.json();
+
+		if (url.pathname === "/") {
+			const paths = Object.keys(json.paths).filter(
+				(p) => p !== "/" && p !== "/rpc/login",
+			);
+			if (!paths.includes(url.pathname)) {
+				return new Response(JSON.stringify({ paths }), {
+					headers: { "Content-Type": "application/json" },
+				});
+			}
+		}
+
+		return new Response(JSON.stringify(json), {
+			headers: { "Content-Type": "application/json" },
+		});
+	} catch (error) {
+		return new Response(
+			JSON.stringify({ message: error.message }),
+			header_internal_server_error,
+		);
+	}
 });
 
 // To invoke:
