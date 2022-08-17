f2fd6b6b1ba1ff665c0d04a751a193880c142e9a
ff6347 <hash@ff6347.email>
2022-08-17 15:11:17 +0200

    style: Use tabs again

--- supabase/migrations/20220816131725_profiles_trigger.sql
+++ supabase/migrations/20220816131725_profiles_trigger.sql
@@ -1,44 +1,44 @@
 SET check_function_bodies = OFF;
 
 CREATE OR REPLACE FUNCTION public.authorizate (requested_permission app_permission, user_id uuid)
-    RETURNS boolean
-    LANGUAGE plpgsql
-    SECURITY DEFINER
-    AS $function$
+	RETURNS boolean
+	LANGUAGE plpgsql
+	SECURITY DEFINER
+	AS $function$
 DECLARE
-    bind_permissions int;
+	bind_permissions int;
 BEGIN
-    SELECT
-        count(*)
-    FROM
-        public.permissions
-        INNER JOIN public.user_roles ON role_permissions.role = user_roles.role
-    WHERE
-        role_permissions.permission = authorize.requested_permission
-        AND user_roles.user_id = authorize.user_id INTO bind_permissions;
-    RETURN bind_permissions > 0;
+	SELECT
+		count(*)
+	FROM
+		public.permissions
+		INNER JOIN public.user_roles ON role_permissions.role = user_roles.role
+	WHERE
+		role_permissions.permission = authorize.requested_permission
+		AND user_roles.user_id = authorize.user_id INTO bind_permissions;
+	RETURN bind_permissions > 0;
 END;
 $function$;
 
 CREATE OR REPLACE FUNCTION public.handle_new_user ()
-    RETURNS TRIGGER
-    LANGUAGE plpgsql
-    SECURITY DEFINER
-    AS $function$
+	RETURNS TRIGGER
+	LANGUAGE plpgsql
+	SECURITY DEFINER
+	AS $function$
 BEGIN
-    INSERT INTO public.profiles (id)
-        VALUES (NEW.id);
-    -- select count(*) = 1
-    -- from auth.users into is_admin;
-    INSERT INTO public.user_roles (user_id, ROLE)
-        VALUES (NEW.id, 'viewer');
-    RETURN new;
+	INSERT INTO public.profiles (id)
+		VALUES (NEW.id);
+	-- select count(*) = 1
+	-- from auth.users into is_admin;
+	INSERT INTO public.user_roles (user_id, ROLE)
+		VALUES (NEW.id, 'viewer');
+	RETURN new;
 END;
 $function$;
 
 -- trigger the function every time a user is created
 CREATE TRIGGER on_auth_user_created
-    AFTER INSERT ON auth.users
-    FOR EACH ROW
-    EXECUTE PROCEDURE public.handle_new_user ();
+	AFTER INSERT ON auth.users
+	FOR EACH ROW
+	EXECUTE PROCEDURE public.handle_new_user ();
 
--- supabase/migrations/20220817101438_rls_permission_system.sql
+++ supabase/migrations/20220817101438_rls_permission_system.sql
@@ -3,10 +3,10 @@ ALTER TABLE "public"."role_permissions" ENABLE ROW LEVEL SECURITY;
 ALTER TABLE "public"."user_roles" ENABLE ROW LEVEL SECURITY;
 
 CREATE POLICY "Enable read access for all users" ON "public"."role_permissions" AS permissive
-    FOR SELECT TO public
-        USING (TRUE);
+	FOR SELECT TO public
+		USING (TRUE);
 
 CREATE POLICY "Enable read access for all users" ON "public"."user_roles" AS permissive
-    FOR SELECT TO public
-        USING (TRUE);
+	FOR SELECT TO public
+		USING (TRUE);
 
