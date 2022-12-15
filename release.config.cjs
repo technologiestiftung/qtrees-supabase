5cf1d11b2fdc75b51b3ed8640580814c67052c15
ff6347 <hash@ff6347.email>
2022-12-15 13:44:14 +0100

    ci(release config): Add shared release config

--- release.config.cjs
+++ release.config.cjs
@@ -1,16 +1,17 @@
 module.exports = {
-	branches: [
-		{ name: "main" },
-		{ name: "staging", prerelease: true }, // `prerelease` is built with the template `${name.replace(/^pre\\//g, "")}`
-	],
-	npmPublish: false,
-	dryRun: false,
-	plugins: [
-		"@semantic-release/commit-analyzer",
-		"@semantic-release/release-notes-generator",
-		"@semantic-release/changelog",
-		"@semantic-release/npm",
-		"@semantic-release/git",
-		"@semantic-release/github",
-	],
+  extends: "@technologiestiftung/semantic-release-config",
+
+  branches: [
+    { name: "main" },
+    { name: "staging", prerelease: true }, // `prerelease` is built with the template `${name.replace(/^pre\\//g, "")}`
+  ],
+  plugins: [
+    [
+      "@saithodev/semantic-release-backmerge",
+      {
+        branch: ["staging"],
+        backmergeStrategy: "merge",
+      },
+    ],
+  ],
 };
